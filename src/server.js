6eec8f6189b3cb6e3888a1c09a673a5c0b9a4b1e
Nikhil-Kumaran <nikhil.kumaran.2011@gmail.com>
2019-09-07 01:34:40 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,36 @@
+
+const WebSocket = require('ws');
+
+const port = process.env.PORT || 8999
+
+
+const wss = new WebSocket.Server({ port: port });
+
+console.log(`Waiting for connection in port ${port}...`);
+
+wss.on('connection', (ws) => {
+
+    console.log('No of clients: ',wss.clients.size)
+    
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+        if(message === 'ping'){
+            ws.send('pong')
+        }
+        else{
+            broadcast(message, ws);
+        }
+        
+    });
+});
+
+function broadcast(message, ws) {
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            if(client !== ws)
+                client.send(message);
+            else
+                client.send(JSON.stringify({client:'You',message: JSON.parse(message).message}));
+        }
+    });
+}
